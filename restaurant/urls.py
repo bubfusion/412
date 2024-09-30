5fe47bc1ae049249487caff6eba7124ed84200de
Brenton <babbb@bu.edu>
2024-09-30 10:17:45 -0400

    Test to see if this fixes gradescope

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 # URL paths for restaurant
 urlpatterns = [
+  path(r'', views.main, name = "main"), #homepage goes to main
   path(r'main', views.main, name = "main"), #main page of Zeno's
   path(r'order', views.order, name = "order"), #order page for food
   path(r'confirmation', views.confirmation, name = "confirmation"), #order confirmation page
