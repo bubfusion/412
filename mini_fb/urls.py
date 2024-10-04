ecfb698c1830bfbb52145c4d09963e60c2c75eb7
Brenton <babbb@bu.edu>
2024-10-04 17:36:40 -0400

    Fixed template and added /mini_fb/mini_fb

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -5,4 +5,5 @@ from . import views
 # URL paths for mini_fb
 urlpatterns = [
   path(r'', views.ShowAllView.as_view(), name = "show_all"),
+  path(r'mini_fb', views.ShowAllView.as_view(), name = "show_all"),
 ]
--- mini_fb/views.py
+++ mini_fb/views.py
@@ -9,5 +9,5 @@ from .models import *
 class ShowAllView(ListView):
   '''Class for the /mini_fb page that shows all users'''
   model = Profile
-  template_name = 'mini_fb/show_all.html'
+  template_name = 'mini_fb/show_all_profiles.html'
   context_object_name = 'profiles'
